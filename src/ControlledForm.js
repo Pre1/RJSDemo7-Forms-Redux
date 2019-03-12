7beb42489226481bc131c28599b2071749ca021c
Octowl <alsaff1987@gmail.com>
2019-03-12 11:13:14 +0300

    Fix typo

--- src/ControlledForm.js
+++ src/ControlledForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import { connect } from "react-redux";
-import * as actionCreatores from "./store/actions/index";
+import * as actionCreators from "./store/actions";
 
 class ControlledForm extends Component {
   render() {
