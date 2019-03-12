8094035f44e47767d869d72acf5e771f6881149d
Octowl <alsaff1987@gmail.com>
2019-03-12 11:11:58 +0300

    Create combined reducer

--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
-import { createStore, compose, applyMiddleware } from "redux";
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
-import postReducer from "./store/reducers/post";
+import peopleReducer from "./store/reducers/people";
+
+const rootReducer = combineReducers({
+  rootPeople: peopleReducer
+});
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  postReducer,
+  rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
 
